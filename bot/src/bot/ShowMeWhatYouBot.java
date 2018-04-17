fed480ea35a6f2ae43be626d4d3c56e45f1c2820
Andrew <andrewrfindlay@ntlworld.com>
2018-04-17 10:47:41 +0100

    Added harvesting bot based on AbstractionLayerAI

--- bot/src/bot/ShowMeWhatYouBot.java
+++ bot/src/bot/ShowMeWhatYouBot.java
@@ -4,53 +4,144 @@
  */
 package bot;
 
+import ai.abstraction.AbstractAction;
+import ai.abstraction.AbstractionLayerAI;
+import ai.abstraction.Harvest;
+import ai.abstraction.pathfinding.AStarPathFinding;
 import ai.core.AI;
 import ai.core.ParameterSpecification;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
+
 import rts.*;
+import rts.units.Unit;
+import rts.units.UnitType;
 import rts.units.UnitTypeTable;
 
 /**
  *
  * @author newtoto
  */
-public class ShowMeWhatYouBot extends AI {    
-    public ShowMeWhatYouBot(UnitTypeTable utt) {
+public class ShowMeWhatYouBot extends AbstractionLayerAI {
+	
+	Random r = new Random();
+	protected UnitTypeTable utt;
+	UnitType workerType;
+	UnitType baseType;
+	UnitType barracksType;
+	UnitType rangedType;
+	UnitType lightType;
+	UnitType heavyType;
+	
+    public ShowMeWhatYouBot(UnitTypeTable a_utt) {
+    	this(a_utt, new AStarPathFinding());
     }
     
 
-    public ShowMeWhatYouBot() {
+    public ShowMeWhatYouBot(UnitTypeTable utt, AStarPathFinding aStarPathFinding) {
+    	super(aStarPathFinding);
+    	reset(utt);
     }
     
     
-    @Override
     public void reset() {
+    	super.reset();
     }
-
     
-    @Override
+    public void reset(UnitTypeTable a_utt) {
+        utt = a_utt;
+        workerType = utt.getUnitType("Worker");
+        baseType = utt.getUnitType("Base");
+        barracksType = utt.getUnitType("Barracks");
+        rangedType = utt.getUnitType("Ranged");
+        lightType = utt.getUnitType("Light");
+    }
+    
     public AI clone() {
-        return new ShowMeWhatYouBot();
+        return new ShowMeWhatYouBot(utt, pf);
     }
    
     
-    @Override
     public PlayerAction getAction(int player, GameState gs) {
-        try {
-            if (!gs.canExecuteAnyAction(player)) return new PlayerAction();
-            PlayerActionGenerator pag = new PlayerActionGenerator(gs, player);
-            return pag.getRandom();
-        }catch(Exception e) {
-            // The only way the player action generator returns an exception is if there are no units that
-            // can execute actions, in this case, just return an empty action:
-            // However, this should never happen, since we are checking for this at the beginning
-            return new PlayerAction();
+    	PhysicalGameState pgs = gs.getPhysicalGameState();
+    	Player p = gs.getPlayer(player);
+
+        List<Unit> workers = new LinkedList<Unit>();
+        for (Unit u : pgs.getUnits()) {
+            if (u.getType().canHarvest
+                    && u.getPlayer() == player) {
+                workers.add(u);
+            }
         }
+        
+        workerBehavior(workers, p, pgs);
+
+        return translateActions(player, gs);
     }
+
+    public void workerBehavior(List<Unit> workers, Player p, PhysicalGameState pgs)
+	{
+		int baseNum = 0;
+		int barrackNum = 0;
+		int usedResourceNum = 0;
+		
+		List<Unit> freeWorkers = new LinkedList<Unit>();
+		freeWorkers.addAll(workers);
+		
+		if(workers.isEmpty()) {return;}
+		
+		for (Unit u2 : pgs.getUnits())
+		{
+			if(u2.getType() == baseType && u2.getPlayer() == p.getID()) {baseNum++;}
+			if(u2.getType() == barracksType && u2.getPlayer() == p.getID()) {barrackNum++;}
+		}
+		
+		for (Unit u : freeWorkers)
+		{
+			Unit closestBase = null;
+			Unit closestResource = null;
+			int closestDistance = 0;
+			for(Unit u2 : pgs.getUnits())
+			{
+				if(u2.getType().isResource)
+				{
+					int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
+					if(closestResource == null || d < closestDistance)
+					{
+						closestResource = u2;
+						closestDistance = d;
+					}
+				}
+			}
+			closestDistance = 0;
+			for(Unit u2 : pgs.getUnits())
+			{
+				if(u2.getType().isStockpile && u2.getPlayer() == p.getID())
+				{
+					int d = Math.abs(u2.getX() - u.getX()) + Math.abs(u2.getY() - u.getY());
+					if(closestBase == null || d < closestDistance)
+					{
+						closestBase = u2;
+						closestDistance = d;
+					}
+				}
+			}
+			
+			if(closestResource != null && closestBase != null)
+			{
+				AbstractAction aa = getAbstractAction(u);
+				if(aa instanceof Harvest)
+				{
+					Harvest h_aa = (Harvest)aa;
+					if(h_aa.getTarget() != closestResource || h_aa.getBase() != closestBase) {harvest (u, closestResource, closestBase);}
+				}else {harvest(u, closestResource, closestBase);}
+			}
+		}
+		
+	}
     
-    
-    @Override
     public List<ParameterSpecification> getParameters()
     {
         return new ArrayList<>();
