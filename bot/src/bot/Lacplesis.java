fb906c96cd7798ad5ac0b0eb58c565a4ff68a8af
RO193540 <RO193540@052TGADS-60404.staff.fal.ac.uk>
2018-04-19 13:02:32 +0100

    Test

--- bot/src/bot/Lacplesis.java
+++ bot/src/bot/Lacplesis.java
@@ -15,12 +15,12 @@ import rts.units.UnitTypeTable;
  *
  * @author santi
  */
-public class RandomAI extends AI {    
-    public RandomAI(UnitTypeTable utt) {
+public class Lacplesis extends AI {    
+    public Lacplesis(UnitTypeTable utt) {
     }
     
 
-    public RandomAI() {
+    public Lacplesis() {
     }
     
     
@@ -31,7 +31,7 @@ public class RandomAI extends AI {
     
     @Override
     public AI clone() {
-        return new RandomAI();
+        return new Lacplesis();
     }
    
     
